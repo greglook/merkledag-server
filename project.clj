180f038d4a52037acb1130855b2979b628ffdbbf
Greg Look <greg@greg-look.net>
2015-10-16 18:02:01 -0700

    Update dependency versions.

--- project.clj
+++ project.clj
@@ -7,19 +7,19 @@
   :deploy-branches ["master"]
 
   :dependencies
-  [[ch.qos.logback/logback-classic "1.1.2"]
-   [compojure "1.3.1"]
-   [com.stuartsierra/component "0.2.2"]
-   [environ "1.0.0"]
+  [[ch.qos.logback/logback-classic "1.1.3"]
+   [compojure "1.4.0"]
+   [com.stuartsierra/component "0.3.0"]
+   [environ "1.0.1"]
    [org.clojure/clojure "1.7.0"]
    [org.clojure/tools.logging "0.3.1"]
-   [ring/ring-core "1.3.2"]
-   [ring/ring-jetty-adapter "1.3.2"]
-   [ring-middleware-format "0.4.0"]]
+   [ring/ring-core "1.4.0"]
+   [ring/ring-jetty-adapter "1.4.0"]
+   [ring-middleware-format "0.6.0"]]
 
   :profiles
   {:repl {:source-paths ["dev/src"]
-          :dependencies [[org.clojure/tools.namespace "0.2.8"]]
+          :dependencies [[org.clojure/tools.namespace "0.2.10"]]
           :jvm-opts ["-DAPP_LOG_APPENDER=repl"
                      "-DAPP_LOG_LEVEL=DEBUG"]}
    :test {:jvm-opts ["-DAPP_LOG_APPENDER=nop"
