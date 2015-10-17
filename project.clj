155e91106dfbb9d5c7c275d4893cf9d95d0b7bc9
Greg Look <greg@greg-look.net>
2015-10-16 17:58:23 -0700

    Add logging config and app dependencies.

--- project.clj
+++ project.clj
@@ -7,4 +7,20 @@
   :deploy-branches ["master"]
 
   :dependencies
-  [[org.clojure/clojure "1.7.0"]])
+  [[ch.qos.logback/logback-classic "1.1.2"]
+   [compojure "1.3.1"]
+   [com.stuartsierra/component "0.2.2"]
+   [environ "1.0.0"]
+   [org.clojure/clojure "1.7.0"]
+   [org.clojure/tools.logging "0.3.1"]
+   [ring/ring-core "1.3.2"]
+   [ring/ring-jetty-adapter "1.3.2"]
+   [ring-middleware-format "0.4.0"]]
+
+  :profiles
+  {:repl {:source-paths ["dev/src"]
+          :dependencies [[org.clojure/tools.namespace "0.2.8"]]
+          :jvm-opts ["-DAPP_LOG_APPENDER=repl"
+                     "-DAPP_LOG_LEVEL=DEBUG"]}
+   :test {:jvm-opts ["-DAPP_LOG_APPENDER=nop"
+                     "-DAPP_LOG_LEVEL=TRACE"]}})
