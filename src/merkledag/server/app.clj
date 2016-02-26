cdf49de7a7214401be89da323b08a8c77caa6282
Greg Look <greg@greg-look.net>
2016-02-25 22:31:18 -0800

    Move routing handler logic to routes ns.

--- src/merkledag/server/app.clj
+++ src/merkledag/server/app.clj
@@ -2,15 +2,11 @@
   "This namespace defines a Ring handler binding URL paths to data lookup and
   controller actions."
   (:require
-    [bidi.bidi :as bidi]
     (merkledag.server
       [response :refer :all]
       [routes :as route])
     (merkledag.server.handlers
       [blocks :refer [block-handlers]])))
-
-
-(def server-root "http://localhost:8080")
 
 
 (defn node-handlers
@@ -30,15 +26,7 @@
 
 (defn ring-handler
   "Constructs a new Ring handler implementing the application."
-  [repo]
-  (let [handlers (merge (block-handlers (:store repo) (str server-root "/blocks/"))
-                        (node-handlers (:store repo)))]
-    (fn handler
-      [request]
-      (let [route (bidi/match-route route/routes (:uri request))]
-        (if-let [method-map (and route (get handlers (:handler route)))]
-          (if-let [handler (or (get method-map (:request-method request))
-                               (:any method-map))]
-            (handler (assoc request :route-params (:route-params route)))
-            (method-not-allowed (keys method-map)))
-          (not-found "No such resource"))))))
+  [repo root-url]
+  (-> (merge (block-handlers (:store repo) (str server-root "/blocks/"))
+             (node-handlers (:store repo)))
+      (route/route-handler)))
--- src/merkledag/server/routes.clj
+++ src/merkledag/server/routes.clj
@@ -2,6 +2,7 @@
   "Route definition for the application."
   (:require
     [bidi.bidi :as bidi]
+    [merkledag.server.response :refer [method-not-allowed not-found]]
     [multihash.core :as multihash]))
 
 
@@ -15,6 +16,9 @@
                    [:id "/data"] :node/data}]]])
 
 
+
+;; ## Path Helpers
+
 (defn block-path
   [id]
   (bidi/path-for routes :block/resource :id (multihash/base58 id)))
@@ -23,3 +27,20 @@
 (defn node-path
   [id]
   (bidi/path-for routes :node/resource :id (multihash/base58 id)))
+
+
+
+;; ## Routing Handler
+
+(defn route-handler
+  "Constructs a new Ring handler which routes to the given map of handlers."
+  [handlers]
+  (fn handler
+    [request]
+    (let [route (bidi/match-route route/routes (:uri request))]
+      (if-let [method-map (and route (get handlers (:handler route)))]
+        (if-let [handler (or (get method-map (:request-method request))
+                             (:any method-map))]
+          (handler (assoc request :route-params (:route-params route)))
+          (method-not-allowed (keys method-map)))
+        (not-found "No such resource")))))
